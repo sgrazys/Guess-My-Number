00636169856916aa21ddaf1d15364d5573892d92
sgrazys <s.grazys@gmail.com>
2023-03-27 23:46:54 +0300

    DOM manipulating - Styles

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'Select a number from 1 to 20'
+
+    //When palyer wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct Number!'
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!'
@@ -23,6 +31,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
 
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
@@ -34,7 +43,5 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
   }
-
-
 
 });
